a487c2a31775fac2beecbe79c7f89f1ae6e48fed
Ramones C3po <ramonesc3po@gmail.com>
2019-09-24 16:50:04 -0300

    Add example

---
+++ example/cloudfront_new_site.tf
@@ -0,0 +1,9 @@
+module "new_site" {
+  source = "../" # source = "github.com/ramonesc3po/terraform-aws-cloudfront-combo-site.git"
+
+  tier                            = "production"
+  organization                    = "example"
+  region                          = "us-east-1"
+  aliases                         = ["new_site.com"] # even if you don't use cloudfront certificate put the alias.
+  certificate_deafault_cloudfront = true
+}
